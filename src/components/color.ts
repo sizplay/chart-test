9c12401ed80555b2569a52070520c581458adc0a
chaehoon <chaehoon_lim@kolon.com>
2023-08-14 15:43:28 +0900

    add recharts chart

---
+++ src/components/color.ts
@@ -0,0 +1,9 @@
+export const color = {
+  background: '#181c31',
+  gridLine: '#2a3247',
+  gradientLow: '#323b55',
+  gradientHigh: '#272f3e',
+  areaGradientLow: '#6873c7',
+  gray: '#b9bbc6',
+  white: '#fff',
+};
