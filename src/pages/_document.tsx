408b5751102de21bbc5cae3871942da2a6e4e1c6
chaehoon <chaehoon_lim@kolon.com>
2023-12-13 17:05:29 +0900

    add code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (co,de,n,but,t,e,r){!n[co]&&(n[co]=function(){
+  (n[co].q=n[co].q||[]).push(arguments);});e=t.createElement(but);
+  e.async=true;e.src=de;r=t.getElementsByTagName(but)[0];
+  r.parentNode.insertBefore(e, r);
+  })("CodenButter", "https://buttr.dev/butter.js", window, "script", document);
+  window.CodenButter("boot", { siteId: "lyqeovifxc", auto: true })
+  `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
